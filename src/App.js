bc98af88877f69113d7962c41e5aee148b9eb77c
MoReza111 <mz.abdollahi223@gmail.com>
2021-05-04 12:58:17 +0430

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import {useEffect} from 'react'
-
 import './App.css'
-import 'materialize-css/dist/css/materialize.min.css'
-import M from 'materialize-css/dist/js/materialize.min.js'
 
-function App() {
-  useEffect(() => {
-    M.AutoInit() // Init Materialize JS
-  })
-
+const App = ()=> {
   return (
     <div className="App">
-
     </div>
   )
 }
